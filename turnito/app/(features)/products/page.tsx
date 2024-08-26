ac54b9e9eb512c92d02d31a9493d1f828c85c9e6
Julian Gallardo <juliangallardo26603@gmail.com>
2024-08-26 14:10:20 -0300

    fix ?

--- turnito/app/(features)/products/page.tsx
+++ turnito/app/(features)/products/page.tsx
@@ -1,10 +1,8 @@
-'use client'
-
 import { title, subtitle } from "@/components/primitives";
 import Card from "@/components/products/itemCard";
 import { Suspense } from "react";
 
-export default async function Home() {
+export default function Home() {
   return (
     <section className="flex flex-col mx-10 gap-4 py-8 md:py-10 overflow-hidden">
       <h1 className={title({ size: "sm" })}>Nuestros productos</h1>
