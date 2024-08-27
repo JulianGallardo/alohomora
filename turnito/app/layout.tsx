c7a1c7090fe270e9ef86c3ba9499c257494fc6b9
Julian Gallardo <juliangallardo26603@gmail.com>
2024-08-26 21:24:26 -0300

    fix defaultTheme

--- turnito/app/layout.tsx
+++ turnito/app/layout.tsx
@@ -24,7 +24,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
 };
 
@@ -42,7 +41,7 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl px-6 flex-grow">
